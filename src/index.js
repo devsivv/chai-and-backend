3b952bfd8b6b162a33711260d1b54e11b80c6010
devsivv <shivamdubey9040@gmail.com>
2025-12-23 15:38:56 +0530

    connected to mongoDB

---
+++ src/index.js
@@ -0,0 +1,45 @@
+// require("dotenv").config({path: "./env"});
+
+
+import dotenv from "dotenv";  
+import mongoose, { Mongoose } from "mongoose";
+import { DB__NAME } from "./constant.js";
+// databse is always in another continent so it takes time to connect so use async await
+// database connection causes problems so use fetch timeout or promise timeout
+
+/* 2nd approach */
+
+import connectDB from "./db/index.js";
+
+dotenv.config({ 
+  path: './env'
+});
+
+
+connectDB();
+
+
+
+
+/* first approach
+import express from "express";
+const app = express();
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGO_URI}/${DBNAME}`)
+    app.on("error", (error) => {
+      console.log("Error:" , error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    });
+
+  } catch (error) {
+    console.log("Error:" , error);
+    throw error;
+  }
+})();
+*/
